ed93c4966f0662d8e31812dac1bc75ed1fd4e4a5
Lorenzo Affetti <affox90@gmail.com>
2014-12-18 15:31:41 +0100

    cmd update

--- shared/cmd.sh
+++ shared/cmd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-service mysql start
-service rabbitmq-server start
+function start_service {
+	if [ -f "/etc/init.d/$1" ]; then
+		#the service is there
+		sudo service $1 start
+	fi
+}
+
+start_service mysql
+start_service rabbitmq-server
 su stack -c '/devstack/stack.sh'
