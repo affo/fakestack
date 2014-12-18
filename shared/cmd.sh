2541b1fa3e58aebfbe7a4ba2fc5cd186b1c339fa
Lorenzo Affetti <affox90@gmail.com>
2014-12-18 17:13:31 +0100

    fix

--- shared/cmd.sh
+++ shared/cmd.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+whoami
+
 function start_service {
 	if [ -f "/etc/init.d/$1" ]; then
 		#the service is there
 		sudo service $1 start
 	fi
 }
+
+if [ $(hostname) = "controller" ]; then
+	# we are in the controller:
+	# change ip
+	sudo ifconfig eth0 42.42.255.254
+fi
 
 start_service mysql
 start_service rabbitmq-server
