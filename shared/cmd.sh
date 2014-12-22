47cd5b6ee86070b4a98147d114ad2f1ca2c30422
Lorenzo Affetti <affox90@gmail.com>
2014-12-22 09:20:30 +0100

    fix

--- shared/cmd.sh
+++ shared/cmd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-whoami
-
 function start_service {
 	if [ -f "/etc/init.d/$1" ]; then
 		#the service is there
@@ -9,10 +7,12 @@ function start_service {
 	fi
 }
 
+CTRL_IP=42.42.255.254/16
 if [ $(hostname) = "controller" ]; then
 	# we are in the controller:
 	# change ip
-	sudo ifconfig eth0 42.42.255.254
+	sudo ifconfig eth0 $CTRL_IP
+	echo "controller: eth0 IP set to $CTRL_IP"
 fi
 
 start_service mysql
