89ff8a8092412aca70080a708cd01f1769b1aff8
Lorenzo Affetti <affox90@gmail.com>
2015-01-04 16:46:01 +0100

    install rabbit and mysql manually if on controller

--- shared/reinstall_service.sh
+++ shared/reinstall_service.sh
@@ -23,5 +23,18 @@ echo "   ---> DevStack cleaned!"
 # remove $1 folder
 rm -rf /opt/stack/$1
 echo "   --> $1 folder removed!"
+
+if [ $(hostname) = "controller" ]; then
+	# reinstall mysql and rabbit.
+	# clean.sh removed them...
+	echo 'mysql-server mysql-server/root_password password pwstack' | debconf-set-selections
+	echo 'mysql-server mysql-server/root_password_again password pwstack' | debconf-set-selections
+	apt-get -qqy install mysql-server
+	apt-get -qqy install rabbitmq-server
+	# and start them
+	sudo service mysql start
+	sudo service rabbitmq-server start
+fi
+
 # reinstall (only $1)
 /devstack/stack.sh
