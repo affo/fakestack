be090306c0601ed8ad41fab69e14fa9486929fb0
Lorenzo Affetti <affox90@gmail.com>
2015-02-19 14:33:51 +0100

    back to old ips; usage part out of cmd

--- shared/cmd.sh
+++ shared/cmd.sh
@@ -13,33 +13,20 @@ function start_service {
 	fi
 }
 
-function post_data {
-	echo 'Retrieving usage data...'
-	i=0
-	avg_cpu=0
-	avg_ram=0
-	ram_total=$(cat /proc/meminfo | awk '/MemTotal/ {print $2}')
-	for i in {1..10}; do
-		cpu=$(mpstat | awk 'FNR == 4 {print $3}')
-		ram_free=$(cat /proc/meminfo | awk '/MemFree/ {print $2}')
-		ram_used=$(($ram_total - $ram_free))
-		i=$(($i + 1))
-		avg_cpu=$(bc <<< 'scale = 5; ($avg_cpu + $cpu) / $i')
-		avg_ram=$(bc <<< 'scale = 5; ($avg_ram + $ram_used) / $i')
-		sleep 1
-	done
-
-	ram_perc=$(bc <<< 'scale = 5; $avg_ram / $ram_total')
-	data='{"cpu_r_used": '$avg_cpu', "ram_r_used: "'$ram_perc'}'
-	curl -X PUT -d $data $FIREBASE_BASE_URL'/'$IP'/usage/'$(date +"%m_%d_%Y__%H_%S")'.json'
-}
-
 ######### cmd
-echo 'installing mpstat...'
-sudo apt-get install -y sysstat
-
+CTRL_IP=42.42.255.254/16
+GW_IP=42.42.0.1
 if [ $(hostname) = "controller" ]; then
 	NODE_TYPE='ctrl'
+	# we are in the controller:
+	# change ip
+	sudo ifconfig eth0 $CTRL_IP
+	echo "controller: eth0 IP set to $CTRL_IP"
+	sudo route add default gw $GW_IP eth0
+	echo "default gateway set to $GW_IP"
+else
+	my_ip=$(ifconfig  | grep 'inet addr:'| grep '42.42.*' | cut -d: -f2 | awk '{ print $1}')
+	sudo sh -c "echo '$my_ip $(hostname)' >> /etc/hosts"
 fi
 
 # test connection with Google's DNS
@@ -62,7 +49,59 @@ ex_time=$(($start-$end))
 #post it
 data='{"time_s": '$ex_time'}'
 curl -X PUT -d '$data' $FIREBASE_BASE_URL'/stack_sh/'$NODE_TYPE'/'$(date +"%m_%d_%Y__%H_%S")'.json'
-post_data
+# change escape sequence
+stty intr \^k
+echo "To KILL the process press CTRL-K"
+# run shell to stay on
+sudo su stack
+
+
+#!/bin/bash
+
+function start_service {
+	if [ -f "/etc/init.d/$1" ]; then
+		#the service is there
+		sudo service $1 start
+	else
+		echo "Cannot start $1: the service is not installed"
+	fi
+}
+
+# editing /etc/hosts for rabbit and openstack
+sudo sh -c "echo '127.0.0.1 localhost' > /etc/hosts"
+sudo sh -c "echo '42.42.255.254 controller' >> /etc/hosts"
+
+CTRL_IP=42.42.255.254/16
+GW_IP=42.42.0.1
+if [ $(hostname) = "controller" ]; then
+	# we are in the controller:
+	# change ip
+	sudo ifconfig eth0 $CTRL_IP
+	echo "controller: eth0 IP set to $CTRL_IP"
+	sudo route add default gw $GW_IP eth0
+	echo "default gateway set to $GW_IP"
+else
+	my_ip=$(ifconfig  | grep 'inet addr:'| grep '42.42.*' | cut -d: -f2 | awk '{ print $1}')
+	sudo sh -c "echo '$my_ip $(hostname)' >> /etc/hosts"
+fi
+
+echo "--> /etc/hosts edited!"
+echo "--> cat /etc/hosts:"
+cat /etc/hosts
+
+# test connection with Google's DNS
+echo "checking connection..."
+check_connection=$(ping -c 3 8.8.8.8)
+if [ -z "$check_connection" ]; then
+	echo "something went wrong in configuring connection..."
+	exit 1
+else
+	echo "connection OK."
+fi
+
+start_service mysql
+start_service rabbitmq-server
+su stack -c '/devstack/stack.sh'
 # change escape sequence
 stty intr \^k
 echo "To KILL the process press CTRL-K"
