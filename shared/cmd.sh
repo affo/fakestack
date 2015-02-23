404829522018d4ae3c69a05eb9be1526ca2be0ca
Lorenzo Affetti <affox90@gmail.com>
2015-02-23 12:02:00 +0100

    HOST -> DISCR

--- shared/cmd.sh
+++ shared/cmd.sh
@@ -2,7 +2,7 @@
 
 FIREBASE_BASE_URL='https://adock.firebaseio.com'
 NODE_TYPE='cmp'
-HOST='oasis'
+DISCR='oasis'
 
 function start_service {
 	if [ -f "/etc/init.d/$1" ]; then
@@ -51,7 +51,7 @@ su stack -c '/devstack/stack.sh'
 end=$(date +%s)
 ex_time=$(($end - $start))
 #post it
-curl -X PUT -d '{"time_s": '$ex_time'}' $FIREBASE_BASE_URL'/stack_sh/'$HOST'/'$NODE_TYPE'/'$(hostname)'.json'
+curl -X PUT -d '{"time_s": '$ex_time'}' $FIREBASE_BASE_URL'/stack_sh/'$DISCR'/'$NODE_TYPE'/'$(hostname)'.json'
 # change escape sequence
 stty intr \^k
 echo "To KILL the process press CTRL-K"
